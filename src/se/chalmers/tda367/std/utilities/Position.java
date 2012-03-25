c0688249833b223d261d5400bf74b585f169ec7c
Emil Edholm <emil@edholm.it>
2012-03-25 17:06:50 +0200

    Implemented hashCode, equals and toString for the Position.

--- src/se/chalmers/tda367/std/utilities/Position.java
+++ src/se/chalmers/tda367/std/utilities/Position.java
@@ -2,10 +2,10 @@ package se.chalmers.tda367.std.utilities;
 
 /**
  * Represents a position/coordinate on the game board.
- * @author Unchanged
+ * @author Emil Edholm
  * @date Mar 22, 2012
  */
-public class Position {
+public final class Position {
 	private int x, y;
 	
 	public Position(int x, int y){
@@ -26,7 +26,30 @@ public class Position {
 		return y;
 	}
 	
-	// hashcode, equals and toString goes here.
+	@Override
+	public String toString(){
+		return "(" + x + ", " + y + ")"; 
+	}
 	
+	@Override 
+	public int hashCode(){
+		int result = 17;
+		result = 31 * result + x;
+		result = 31 * result + y;
+		
+		return result;
+	}
 	
+	@Override
+	public boolean equals(Object rhs){
+		if(this == rhs)
+			return true;
+		if(!(rhs instanceof Position))
+			return false;
+		
+		// Safe to cast.
+		Position p = (Position)rhs;
+		return x == p.x && y == p.y;
+	}
+
 }
