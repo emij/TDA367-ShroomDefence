3e5759e874dd771cb75be4bd685142682a076367
Emil Johansson <emij@student.chalmers.se>
2012-03-26 16:44:52 +0200

    Written main to test a textbased version of the gameboard/tiles

--- src/se/chalmers/tda367/std/Main.java
+++ src/se/chalmers/tda367/std/Main.java
@@ -1,5 +1,15 @@
 package se.chalmers.tda367.std;
 
+import java.util.Random;
+
+import se.chalmers.tda367.std.core.GameBoard;
+import se.chalmers.tda367.std.core.tiles.BuildableTile;
+import se.chalmers.tda367.std.core.tiles.IBoardTile;
+import se.chalmers.tda367.std.core.tiles.IBuildableTile;
+import se.chalmers.tda367.std.core.tiles.PathTile;
+import se.chalmers.tda367.std.utilities.Position;
+import se.chalmers.tda367.std.utilities.Sprite;
+
 /**
  * Contains the main method. The entrance to the game.
  * @author Unchanged
@@ -12,8 +22,37 @@ public final class Main {
 	 * @param args the command line arguments.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GameBoard board = new GameBoard(20,20);
+		randomPlaceTile(board);
+		placePath(board);
+		System.out.println(board);
 
 	}
+
+	/**
+	 * @param board
+	 */
+	private static void placePath(GameBoard board) {
+		IBoardTile pathTile = new PathTile(new Sprite());
+		int y = (board.getHeigth()/2)-1
+		for (int i = 0; i < board.getWidth()-1; i++) {
+			board.placeTile(pathTile, new Position(i,y));
+			board.placeTile(pathTile, new Position(i,y+1));
+		}
+	}
+
+	/**
+	 * @param board
+	 */
+	private static void randomPlaceTile(GameBoard board) {
+		Random rnd = new Random();
+		IBoardTile buildTile = new BuildableTile(new Sprite());
+		for (int i = 0; i < 200; i++) {
+			int x = rnd.nextInt(board.getWidth()-1);
+			int y = rnd.nextInt(board.getHeight()-1);
+			board.placeTile(buildTile, new Position(x,y));
+		}
+	}
+	
 
 }
