dc90a50adef5a991f942b4c1269d29e00184814b
Emil Johansson <emij@student.chalmers.se>
2012-03-27 15:11:21 +0200

    Implemented the code for the PlayerBase

--- src/se/chalmers/tda367/std/core/tiles/IPlayerBase.java
+++ src/se/chalmers/tda367/std/core/tiles/IPlayerBase.java
@@ -1,18 +1,40 @@
 package se.chalmers.tda367.std.core.tiles;
 
+import se.chalmers.tda367.std.utilities.Sprite;
+
 
 /**
  * Represents the player base. 
  * i.e. the structure the enemies tries to destroy/get into.
- * @author Unchanged
+ * @author Emil Johansson
  * @date Mar 22, 2012
  */
 public interface IPlayerBase extends IBoardTile{
 	/**
 	 * Returns the health of the player base.
+	 * @return health of the player base
 	 */
 	public int getHealth();
+	/**
+	 * Increase health of the player base with specified value.
+	 * @param inc
+	 * @return health of player base
+	 */
 	public int increaseHealth(int inc);
+	/**
+	 * Decrease health of player base with one.
+	 * @return health of player base
+	 */
 	public int decreaseHealth();
+	/**
+	 * Decrease health of player base with specified value.
+	 * @param dmg
+	 * @return health of player base
+	 */
 	public int decreaseHealth(int dmg);
+	/**
+	 * Returns the graphical representation of the player base.
+	 * @return sprite of the player base
+	 */
+	public Sprite getSprite();
 }
