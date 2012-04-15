5437578315f91cd0cebce6bd797e1cc93a4c0475
Johan Gustafsson <johan.gusten@gmail.com>
2012-04-15 18:54:52 +0200

    Added EnemyItem which represents and enemy, its position on the gameboard and its distance to the end position

---
+++ src/se/chalmers/tda367/std/core/EnemyItem.java
@@ -0,0 +1,42 @@
+package se.chalmers.tda367.std.core;
+
+import se.chalmers.tda367.std.core.tiles.enemies.IEnemy;
+import se.chalmers.tda367.std.utilities.Position;
+
+/**
+ * Represents an enemy, the enemy's position on the gameboard and it's distance to the end position.
+ * @author Johan Gustafsson
+ * @date April 15, 2012
+ */
+class EnemyItem {
+	private IEnemy enemy;
+	private Position enemyPos;
+	private int distanceToGoal;
+	
+	public EnemyItem(IEnemy enemy, Position enemyPos, int distanceToGoal) {
+		this.enemy = enemy;
+		this.enemyPos = enemyPos;
+		this.distanceToGoal = distanceToGoal;
+	}
+	
+	/**
+	 * @return the enemy reference.
+	 */
+	public IEnemy getEnemy() {
+		return enemy;
+	}
+	
+	/**
+	 * @return the Position the enemy have on the gameboard.
+	 */
+	public Position getEnemyPos() {
+		return enemyPos;
+	}
+	
+	/**
+	 * @return the shortest distance between the enemy and the end position.
+	 */
+	public int getDistanceToGoal() {
+		return distanceToGoal;
+	}
+}
