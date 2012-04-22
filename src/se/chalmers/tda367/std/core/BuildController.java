42b81328d03417517a8680c831073dac1d5d58eb
Johan Andersson <horv@telia.com>
2012-04-22 23:02:30 +0200

    Created BuildController to handle build phase logic

---
+++ src/se/chalmers/tda367/std/core/BuildController.java
@@ -0,0 +1,12 @@
+package se.chalmers.tda367.std.core;
+
+/**
+ * The class that contains the game logic for build phase of the game.
+ * @author Johan Andersson
+ * @modified 
+ * @date Apr 22, 2012
+ */
+
+public class BuildController {
+
+}
