0b7c668c5f357ee9077d3f0cbe7c4388631d04af
Johan Gustafsson <johan.gusten@gmail.com>
2012-04-23 20:59:04 +0200

    I have reworked the GUI to now use different states i.e Main Menu, Gameplay. Added background and button pictures.

---
+++ src/se/chalmers/tda367/std/gui/MainMenuState.java
@@ -0,0 +1,98 @@
+package se.chalmers.tda367.std.gui;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import sun.reflect.ReflectionFactory.GetReflectionFactoryAction;
+import sun.swing.BakedArrayList;
+
+public class MainMenuState extends BasicGameState {
+	private int stateID;
+	private Image background;
+	private Image startGameButton;
+	private Image exitGameButton;
+	private float startButtonScale;
+	private float exitButtonScale;
+	private int menuX;
+	private int menuY;
+	
+	public MainMenuState(int stateID) {
+		this.stateID = stateID;
+	}
+	
+	private String getResourcePath(String path) {
+		return getClass().getResource(path).getPath();
+	}
+	
+	@Override
+	public void init(GameContainer container, StateBasedGame state)
+			throws SlickException {
+		background = new Image(getResourcePath("/main_background.png"));
+		startGameButton = new Image(getResourcePath("/main_start_button.png"));
+		startButtonScale = 1;
+		exitGameButton = new Image(getResourcePath("/main_exit_button.png"));
+		exitButtonScale = 1;
+		
+		menuX = container.getWidth()/35;
+		menuY = container.getHeight()/2;
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame state, Graphics g)
+			throws SlickException {
+		background.draw(0, 0, container.getWidth(), container.getHeight());
+		startGameButton.draw(menuX, menuY, startButtonScale);
+		exitGameButton.draw(menuX, menuY+startGameButton.getHeight(), exitButtonScale);
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame state, int arg2)
+			throws SlickException {
+		Input input = container.getInput();
+		int mouseX = input.getMouseX();
+		int mouseY = input.getMouseY();
+		
+		boolean overStartButton = false;
+		boolean overExitButton = false;
+		
+		if((mouseX >= menuX && mouseX <= menuX+startGameButton.getWidth()) && 
+				(mouseY >= menuY && mouseY <= menuY+startGameButton.getHeight())) {
+			overStartButton = true;
+		}
+		else if((mouseX >= menuX && mouseX <= menuX+exitGameButton.getWidth()) && 
+				(mouseY >= menuY+startGameButton.getHeight() && 
+				 mouseY <= menuY+startGameButton.getHeight()+exitGameButton.getHeight())) {
+			overExitButton = true;
+		}
+		if(overStartButton) {
+			startButtonScale = 1.05f;
+			exitButtonScale = 1;
+			if(input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+				state.enterState(STDGame.GAMEPLAYSTATE);
+			}
+		}
+		else if(overExitButton){
+			exitButtonScale = 1.05f;
+			startButtonScale = 1;
+			if(input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+				container.exit();
+			}
+		}
+		else {
+			startButtonScale = 1;
+			exitButtonScale = 1;
+		}
+	}
+
+	@Override
+	public int getID() {
+		return stateID;
+	}
+
+
+}
