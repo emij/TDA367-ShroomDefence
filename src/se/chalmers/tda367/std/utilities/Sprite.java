ae8557febe5c7ece8b8ad7871bf50b2eb30caa79
Emil Edholm <emil@edholm.it>
2012-04-24 22:57:31 +0200

    Implemented a basic Sprite

--- src/se/chalmers/tda367/std/utilities/Sprite.java
+++ src/se/chalmers/tda367/std/utilities/Sprite.java
@@ -1,10 +1,44 @@
 package se.chalmers.tda367.std.utilities;
 
+import java.awt.Image;
+
 /**
- * Represents a Sprite. i.e. a moving image of sorts.
- * @author Unchanged
- * @date Mar 22, 2012
+ * Represents a Sprite. i.e. a moving (or static) image of sorts.
+ * @author Emil Edholm
+ * @date Apr 24, 2012
  */
 public class Sprite {
-	// Not yet decided what needs to go here.
+	public final Image image;
+	
+	/**
+	 * Create an empty sprite with no associated image.
+	 */
+	public Sprite(){
+		image = null;
+	}
+	
+	/**
+	 * Create a sprite with the image loaded from a resource string.
+	 * @param resourceString the resource string from which to load the image.
+	 */
+	public Sprite(String resourceString){
+		this();
+		// TODO: Load image from resources. Possible use of a resource loader.
+	}
+	
+	/**
+	 * Create a sprite from the supplied image.
+	 * @param image the image to base the sprite on.
+	 */
+	public Sprite(Image image){
+		this.image = image;
+	}
+	
+	/**
+	 * Retrieves the image associated with this sprite.
+	 * @return an image representing the sprite.
+	 */
+	public Image getImage() {
+		return image;
+	}
 }
