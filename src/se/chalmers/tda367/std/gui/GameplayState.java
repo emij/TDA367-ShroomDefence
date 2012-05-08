0bc647b6eba4c0c70b99a10525fc468fcdcdaf77
Johan Gustafsson <johan.gusten@gmail.com>
2012-05-08 14:45:12 +0200

    Continued cleaning up and breaking down the GameplayState to make it more readable and easier to understand. Also implemented a better and more logical visual feedback when trying to build a tower

--- src/se/chalmers/tda367/std/gui/GameplayState.java
+++ src/se/chalmers/tda367/std/gui/GameplayState.java
@@ -3,7 +3,6 @@ package se.chalmers.tda367.std.gui;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
@@ -17,11 +16,8 @@ import se.chalmers.tda367.std.core.GameController;
 import se.chalmers.tda367.std.core.Player;
 import se.chalmers.tda367.std.core.Properties;
 import se.chalmers.tda367.std.core.exported.BasicAttackTower;
-import se.chalmers.tda367.std.core.tiles.BuildableTile;
 import se.chalmers.tda367.std.core.tiles.IBoardTile;
 import se.chalmers.tda367.std.core.tiles.IBuildableTile;
-import se.chalmers.tda367.std.core.tiles.PathTile;
-import se.chalmers.tda367.std.core.tiles.towers.ITower;
 import se.chalmers.tda367.std.events.TowerShootingEvent;
 import se.chalmers.tda367.std.utilities.EventBus;
 import se.chalmers.tda367.std.utilities.NativeSprite;
@@ -29,40 +25,28 @@ import se.chalmers.tda367.std.utilities.Position;
 
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.controls.Label;
-import de.lessvoid.nifty.effects.EffectEventId;
 import de.lessvoid.nifty.elements.Element;
-import de.lessvoid.nifty.elements.render.TextRenderer;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 import de.lessvoid.nifty.slick2d.NiftyOverlayBasicGameState;
-import de.lessvoid.nifty.slick2d.input.PlainSlickInputSystem;
-import de.lessvoid.nifty.slick2d.render.SlickRenderDevice;
-import de.lessvoid.nifty.slick2d.sound.SlickSoundDevice;
-import de.lessvoid.nifty.slick2d.time.LWJGLTimeProvider;
-import de.lessvoid.nifty.spi.input.InputSystem;
-import de.lessvoid.nifty.tools.SizeValue;
 
 
 public class GameplayState extends NiftyOverlayBasicGameState implements ScreenController {
 	private int stateID;
-	private Image towerTile;
 	private int tileScale;
+	private int mouseX, mouseY;
 	private boolean towerChoosed;
 	private GameBoard board;
 	private Properties properties = Properties.INSTANCE;
 	private Player player;
 	private GameController gameControl;
 	private Nifty nifty;
-	private int mouseX, mouseY;
-	
+	private Element lifeLabel, scoreLabel;
+
 	public GameplayState(int stateID) {
 		this.stateID = stateID;
 		towerChoosed = false;
 		tileScale = properties.getTileScale();
-	}
-	
-	private String getResourcePath(String path){
-		return getClass().getResource(path).getPath();
 	}
 	
 	@Override
@@ -73,8 +57,6 @@ public class GameplayState extends NiftyOverlayBasicGameState implements ScreenC
 	@Override
 	protected void initGameAndGUI(GameContainer container, StateBasedGame state)
 			throws SlickException {
-		towerTile = new Image(getResourcePath("/images/gameplay/tower_tile1.png"));
-		
 		board = new GameBoard(25,20, GameBoard.BoardPosition.valueOf(0,12), GameBoard.BoardPosition.valueOf(19,12));
 		player = new Player("GustenTestar");
 		gameControl = new GameController(player, board);
@@ -82,6 +64,8 @@ public class GameplayState extends NiftyOverlayBasicGameState implements ScreenC
 		initNifty(container, state);
 		nifty = this.getNifty();
 		prepareNifty(nifty, state);
+		
+		container.getGraphics().setLineWidth(3);
 		
 		EventBus.INSTANCE.register(this);
 	}
@@ -94,38 +78,22 @@ public class GameplayState extends NiftyOverlayBasicGameState implements ScreenC
 	@Override
 	protected void prepareNifty(Nifty nifty, StateBasedGame state) {
 		nifty.fromXml(getResourcePath("/XMLFile1.xml"), "start", this);
+		
+		lifeLabel = nifty.getCurrentScreen().findElementByName("lifeLabel");
+		scoreLabel = nifty.getCurrentScreen().findElementByName("scoreLabel");
 	}
 
 	@Override
 	protected void renderGame(GameContainer container, StateBasedGame state,
 			Graphics g) throws SlickException {
-        g.setColor(Color.black);
+		g.setColor(Color.black);
+        renderStats();
+        renderTiles();
+        renderEnemies(g);
         
-		int w = board.getWidth();
-        int h = board.getHeight();
-        for(int y = 0; y < h; y++){
-        	for(int x = 0; x < w; x++){
-        		IBoardTile tile = board.getTileAt(x, y);
-        		int nX = x * tileScale;
-        		int nY = y * tileScale;
-        		tile.getSprite().getNativeSprite().draw(nX, nY, tileScale, tileScale);
-        	}
-        }
-        
-        //TODO: Fix so this method shows correct tower and has correct placement
         if(towerChoosed) {
-        	towerTile.draw(mouseX, mouseY, tileScale, tileScale);
+        	renderBuildingFeedback(g);
         }
-        
-        for(EnemyItem ei : board.getEnemies() ) {
-        	Position p = ei.getEnemyPos();
-        	int health = ei.getEnemy().getHealth();
-        	
-        	NativeSprite image = ei.getEnemy().getSprite().getNativeSprite();
-        	image.draw(p.getX(), p.getY(), tileScale, tileScale);
-        	g.drawString(""+health, p.getX(), p.getY()-tileScale);
-        }
-        nifty.render(false);
 	}
 	
 	@Subscribe
@@ -140,7 +108,7 @@ public class GameplayState extends NiftyOverlayBasicGameState implements ScreenC
 			throws SlickException {
 		gameControl.updateGameState(delta);
 		nifty.update();
-		UpdateCurrentStats();
+		
 		
 		Input input = container.getInput();
 		mouseX = input.getMouseX();
@@ -168,7 +136,11 @@ public class GameplayState extends NiftyOverlayBasicGameState implements ScreenC
 	public int getID() {
 		return stateID;
 	}
-
+	
+	private String getResourcePath(String path){
+		return getClass().getResource(path).getPath();
+	}
+	
 	@Override
 	public void bind(Nifty nifty, Screen screen) {
 	}
@@ -185,21 +157,46 @@ public class GameplayState extends NiftyOverlayBasicGameState implements ScreenC
 		gameControl.nextWave();
 	}
 	
+	private void renderTiles() {
+        for(int y = 0; y < board.getHeight(); y++){
+        	for(int x = 0; x < board.getWidth(); x++){
+        		IBoardTile tile = board.getTileAt(x, y);
+        		int nX = x * tileScale;
+        		int nY = y * tileScale;
+        		tile.getSprite().getNativeSprite().draw(nX, nY, tileScale, tileScale);
+        	}
+        }
+	}
 	
-	public void UpdateCurrentStats() {
-		Element lifeLabel = nifty.getCurrentScreen().findElementByName("lifeLabel");
-		Element scoreLabel = nifty.getCurrentScreen().findElementByName("scoreLabel");
-		
+	private void renderEnemies(Graphics g) {
+        for(EnemyItem ei : board.getEnemies() ) {
+        	Position p = ei.getEnemyPos();
+        	int health = ei.getEnemy().getHealth();
+        	NativeSprite image = ei.getEnemy().getSprite().getNativeSprite();
+        	
+        	image.draw(p.getX(), p.getY(), tileScale, tileScale);
+        	g.drawString(""+health, p.getX(), p.getY()-tileScale);
+        }
+	}
+	
+	private void renderStats() {
 	    lifeLabel.getNiftyControl(Label.class).setText("" + board.getPlayerBase().getHealth());
 	    scoreLabel.getNiftyControl(Label.class).setText("" + player.getCurrentScore());
 	}
 	
-	public String getPlayerScore() {
-		return player.getCurrentScore() + "";
-	}
-	
-	public String getRemainingLife() {
-		return board.getPlayerBase().getHealth() + "";
+	private void renderBuildingFeedback(Graphics g) {
+		int x = mouseX/tileScale;
+		int y = mouseY/tileScale;
+		if(board.canBuildAt(BoardPosition.valueOf(x, y))) {
+			g.setColor(Color.green);
+			g.drawRect(x * tileScale, y * tileScale, tileScale, tileScale);
+			g.setColor(Color.black);
+		}
+		else {
+			g.setColor(Color.red);
+			g.drawRect(x * tileScale, y * tileScale, tileScale, tileScale);
+			g.setColor(Color.black);
+		}
 	}
 	
 	public void buildTower(String tower) {
