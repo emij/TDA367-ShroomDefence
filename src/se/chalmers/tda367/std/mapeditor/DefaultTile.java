ae267410b8d4b2309e3db9770b6636534832c8ed
Emil Edholm <emil@edholm.it>
2012-05-09 16:11:18 +0200

    Added methods for retrieveing the tile represented by the enum

--- src/se/chalmers/tda367/std/mapeditor/DefaultTile.java
+++ src/se/chalmers/tda367/std/mapeditor/DefaultTile.java
@@ -1,5 +1,8 @@
 package se.chalmers.tda367.std.mapeditor;
 
+import se.chalmers.tda367.std.core.maps.MapItem;
+import se.chalmers.tda367.std.core.tiles.IBoardTile;
+
 /**
  * Represents the choices of default tile placement in the Map Editor new map wizard.
  * @author Emil Edholm
@@ -7,16 +10,22 @@ package se.chalmers.tda367.std.mapeditor;
  */
 public enum DefaultTile {
 	
-	TERRAIN_TILE("Terrain tile"),
-	BUILDABLE_TILE("Buildable tile");
+	TERRAIN_TILE("Terrain tile", MapItem.TERRAIN_TILE),
+	BUILDABLE_TILE("Buildable tile", MapItem.BUILDABLE_TILE);
 	
 	private final String fancyName;
-	private DefaultTile(String fancyName){
+	private final IBoardTile tile;
+	private DefaultTile(String fancyName, IBoardTile tile){
 		this.fancyName = fancyName;
+		this.tile = tile;
 	}
 	
 	@Override
 	public String toString() {
-		return fancyName;
+		return this.fancyName;
+	}
+	
+	public IBoardTile getTile() {
+		return this.tile;
 	}
 }
