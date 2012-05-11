93e5a365ca239435af31ed07073ed1573c67bf26
Emil Edholm <emil@edholm.it>
2012-05-11 22:23:01 +0200

    Added pixel size info when creating a new map

--- src/se/chalmers/tda367/std/mapeditor/NewMapWizard.java
+++ src/se/chalmers/tda367/std/mapeditor/NewMapWizard.java
@@ -17,11 +17,17 @@ import javax.swing.SpinnerNumberModel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 
+import se.chalmers.tda367.std.core.Properties;
 import se.chalmers.tda367.std.mapeditor.events.NewMapEvent;
 import se.chalmers.tda367.std.utilities.EventBus;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 
 @SuppressWarnings("serial")
 public class NewMapWizard extends JDialog {
@@ -31,6 +37,15 @@ public class NewMapWizard extends JDialog {
 	private final JSpinner heightSpinner;
 	private final JSpinner levelSpinner;
 	private final JComboBox<DefaultTile> defaultTileComboBox;
+	private JLabel lblHeight;
+	private JLabel lblDefaultTile;
+	private JLabel lblMapWidth;
+	private JLabel lblMapLevel;
+	private JPanel buttonPane;
+	private JButton okButton;
+	private JButton cancelButton;
+	private JPanel panel;
+	private JLabel lblPixelSize;
 
 	/**
 	 * Create the dialog.
@@ -40,18 +55,18 @@ public class NewMapWizard extends JDialog {
 		setResizable(false);
 		setModalityType(ModalityType.APPLICATION_MODAL);
 		setTitle("New map");
-		setBounds(100, 100, 206, 185);
+		setBounds(100, 100, 275, 185);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		GridBagLayout gbl_contentPanel = new GridBagLayout();
-		gbl_contentPanel.columnWidths = new int[]{0, 0, 0};
+		gbl_contentPanel.columnWidths = new int[]{0, 0, 0, 0};
 		gbl_contentPanel.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gbl_contentPanel.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPanel.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
 		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPanel.setLayout(gbl_contentPanel);
 		{
-			JLabel lblMapWidth = new JLabel("Width");
+			lblMapWidth = new JLabel("Width");
 			lblMapWidth.setFont(new Font("Dialog", Font.PLAIN, 13));
 			lblMapWidth.setToolTipText("The number of tiles used in the width");
 			GridBagConstraints gbc_lblMapWidth = new GridBagConstraints();
@@ -63,16 +78,37 @@ public class NewMapWizard extends JDialog {
 		}
 		{
 			widthSpinner = new JSpinner();
+			widthSpinner.addChangeListener(new ChangeListener() {
+				public void stateChanged(ChangeEvent e) {
+					updatePixelSize();
+				}
+			});
 			widthSpinner.setModel(new SpinnerNumberModel(25, 10, 500, 1));
 			GridBagConstraints gbc_spinner = new GridBagConstraints();
 			gbc_spinner.fill = GridBagConstraints.HORIZONTAL;
-			gbc_spinner.insets = new Insets(0, 0, 5, 0);
+			gbc_spinner.insets = new Insets(0, 0, 5, 5);
 			gbc_spinner.gridx = 1;
 			gbc_spinner.gridy = 0;
 			contentPanel.add(widthSpinner, gbc_spinner);
 		}
 		{
-			JLabel lblHeight = new JLabel("Height");
+			panel = new JPanel();
+			GridBagConstraints gbc_panel = new GridBagConstraints();
+			gbc_panel.gridheight = 2;
+			gbc_panel.insets = new Insets(0, 0, 5, 0);
+			gbc_panel.fill = GridBagConstraints.BOTH;
+			gbc_panel.gridx = 2;
+			gbc_panel.gridy = 0;
+			contentPanel.add(panel, gbc_panel);
+			panel.setLayout(new BorderLayout(0, 0));
+			{
+				lblPixelSize = new JLabel("640x480 px");
+				lblPixelSize.setHorizontalAlignment(SwingConstants.CENTER);
+				panel.add(lblPixelSize);
+			}
+		}
+		{
+			lblHeight = new JLabel("Height");
 			lblHeight.setFont(new Font("Dialog", Font.PLAIN, 13));
 			GridBagConstraints gbc_lblHeight = new GridBagConstraints();
 			gbc_lblHeight.insets = new Insets(0, 0, 5, 15);
@@ -83,16 +119,21 @@ public class NewMapWizard extends JDialog {
 		}
 		{
 			heightSpinner = new JSpinner();
+			heightSpinner.addChangeListener(new ChangeListener() {
+				public void stateChanged(ChangeEvent e) {
+					updatePixelSize();
+				}
+			});
 			heightSpinner.setModel(new SpinnerNumberModel(20, 10, 500, 1));
 			GridBagConstraints gbc_spinner = new GridBagConstraints();
 			gbc_spinner.fill = GridBagConstraints.HORIZONTAL;
-			gbc_spinner.insets = new Insets(0, 0, 5, 0);
+			gbc_spinner.insets = new Insets(0, 0, 5, 5);
 			gbc_spinner.gridx = 1;
 			gbc_spinner.gridy = 1;
 			contentPanel.add(heightSpinner, gbc_spinner);
 		}
 		{
-			JLabel lblDefaultTile = new JLabel("Default tile");
+			lblDefaultTile = new JLabel("Default tile");
 			lblDefaultTile.setFont(new Font("Dialog", Font.PLAIN, 13));
 			GridBagConstraints gbc_lblDefaultTile = new GridBagConstraints();
 			gbc_lblDefaultTile.anchor = GridBagConstraints.EAST;
@@ -107,14 +148,14 @@ public class NewMapWizard extends JDialog {
 			defaultTileComboBox.setToolTipText("What tile to fill the \"background\" with by default");
 			defaultTileComboBox.setModel(new DefaultComboBoxModel<DefaultTile>(DefaultTile.values()));
 			GridBagConstraints gbc_comboBox = new GridBagConstraints();
-			gbc_comboBox.insets = new Insets(0, 0, 5, 0);
 			gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+			gbc_comboBox.insets = new Insets(0, 0, 5, 5);
 			gbc_comboBox.gridx = 1;
 			gbc_comboBox.gridy = 2;
 			contentPanel.add(defaultTileComboBox, gbc_comboBox);
 		}
 		{
-			JLabel lblMapLevel = new JLabel("Map level");
+			lblMapLevel = new JLabel("Map level");
 			lblMapLevel.setToolTipText("What level the new map shall represent.");
 			lblMapLevel.setFont(new Font("Dialog", Font.PLAIN, 13));
 			GridBagConstraints gbc_lblMapLevel = new GridBagConstraints();
@@ -130,16 +171,18 @@ public class NewMapWizard extends JDialog {
 			levelSpinner.setModel(new SpinnerNumberModel(1, 1, 1000, 1));
 			GridBagConstraints gbc_spinner = new GridBagConstraints();
 			gbc_spinner.fill = GridBagConstraints.HORIZONTAL;
+			gbc_spinner.insets = new Insets(0, 0, 0, 5);
 			gbc_spinner.gridx = 1;
 			gbc_spinner.gridy = 3;
 			contentPanel.add(levelSpinner, gbc_spinner);
 		}
+		contentPanel.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{lblHeight, lblDefaultTile, heightSpinner, defaultTileComboBox, widthSpinner, lblMapWidth, lblMapLevel, levelSpinner}));
 		{
-			JPanel buttonPane = new JPanel();
+			buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
+				okButton = new JButton("OK");
 				okButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						// What happens on the OK button click.
@@ -158,7 +201,7 @@ public class NewMapWizard extends JDialog {
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
+				cancelButton = new JButton("Cancel");
 				cancelButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						// Hide form on Cancel click.
@@ -169,6 +212,15 @@ public class NewMapWizard extends JDialog {
 				buttonPane.add(cancelButton);
 			}
 		}
+		updatePixelSize();
+	}
+	
+	private void updatePixelSize() {
+		int width = (int) widthSpinner.getValue();
+		int height = (int) heightSpinner.getValue();
+		int scale = Properties.INSTANCE.getTileScale();
+		
+		lblPixelSize.setText(width*scale + "x" + height*scale);
 	}
 
 }
