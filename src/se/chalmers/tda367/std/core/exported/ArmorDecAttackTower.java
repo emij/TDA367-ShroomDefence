892ccff91def6a73ff84881ff3ce26c83c990016
Emil Johansson <emij@student.chalmers.se>
2012-05-12 20:58:39 +0200

    Implemented a new tower with an armor decreasing effect

---
+++ src/se/chalmers/tda367/std/core/exported/ArmorDecAttackTower.java
@@ -0,0 +1,67 @@
+package se.chalmers.tda367.std.core.exported;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import se.chalmers.tda367.std.core.effects.IEffect;
+import se.chalmers.tda367.std.core.effects.ReduceArmorEffect;
+import se.chalmers.tda367.std.core.tiles.towers.AbstractAttackTower;
+import se.chalmers.tda367.std.utilities.Sprite;
+import se.chalmers.tda367.std.core.anno.Tower;
+import se.chalmers.tda367.std.utilities.SpriteCreator;
+
+/**
+ * Represents a Armor decreasing attack tower for use in a textual game board.
+ * <p>
+ * Base cost        = <b>200</b><br />
+ * Base damage      = <b>5</b><br />
+ * Effective radius = <b>2</b><br />
+ * Damage radius    = <b>0</b><br />
+ * Attack speed     = <b>80</b><br />
+ * </p>
+ * @author Emil Johansson
+ * @date   12 may 2012
+ */
+@Tower(name = "ArmorDec", description = "AttackTower that decrease enemy armor", towerStrength = 2)
+public final class ArmorDecAttackTower extends AbstractAttackTower {
+
+	private static final int baseCost        = 200, 
+							 baseDamage      = 5, 
+							 effectiveRadius = 2, 
+							 aoeRadius       = 0, 
+							 attackSpeed     = 80;
+	private static List<IEffect> effects = new ArrayList<IEffect>();
+	static {
+		effects.add(new ReduceArmorEffect(1));
+	}
+	
+	//TODO: change sprite for the slowing tower.
+	private final static Sprite sprite = SpriteCreator.create("/images/gameplay/slow_tower_tile.png");
+	
+	private int upgradeLevel = 1;
+	private int dmgModifier = 1;
+	
+	public ArmorDecAttackTower() {
+		super(baseCost, baseDamage, effectiveRadius, aoeRadius, attackSpeed,
+				effects, sprite);
+	}
+
+	@Override
+	public void upgrade() {
+		super.upgrade();
+		dmgModifier++;
+	}
+
+	@Override
+	public int getUpgradeCost() {
+		return 10 * upgradeLevel;
+	}
+
+	/**
+	 * The textual representation of the tower on a text based game board.
+	 */
+	@Override
+	public String toString(){
+		return "A";
+	}
+}
