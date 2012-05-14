b946e2b3557003be069a241fadec3e94e1224a6a
Johan Gustafsson <johan.gusten@gmail.com>
2012-05-14 02:27:58 +0200

    Added so the player character can shoot

--- src/se/chalmers/tda367/std/core/IPlayerCharacter.java
+++ src/se/chalmers/tda367/std/core/IPlayerCharacter.java
@@ -1,5 +1,8 @@
 package se.chalmers.tda367.std.core;
 
+import java.util.List;
+
+import se.chalmers.tda367.std.core.enemies.IEnemy;
 import se.chalmers.tda367.std.utilities.Position;
 import se.chalmers.tda367.std.utilities.Sprite;
 
@@ -21,4 +24,17 @@ public interface IPlayerCharacter {
 	 * @return the sprite (image representation) of the character.
 	 */
 	public Sprite getSprite();
+	
+	/**
+	 * Causes the character to attack an enemy.
+	 * @param enemies list of possible enemies to attack
+	 */
+	void shoot(List<IEnemy> enemies);
+	
+	/**
+	 * Checks if the character is ready to attack again.
+	 * @param delta - the time since the last game state update in milliseconds.
+	 * @return true if ready to attack, else false.
+	 */
+	boolean isAttackReady(int delta);
 }
--- src/se/chalmers/tda367/std/core/PlayerCharacter.java
+++ src/se/chalmers/tda367/std/core/PlayerCharacter.java
@@ -1,5 +1,11 @@
 package se.chalmers.tda367.std.core;
 
+import java.util.List;
+
+import se.chalmers.tda367.std.core.effects.IEffect;
+import se.chalmers.tda367.std.core.enemies.IEnemy;
+import se.chalmers.tda367.std.core.events.TowerShootingEvent;
+import se.chalmers.tda367.std.utilities.EventBus;
 import se.chalmers.tda367.std.utilities.Position;
 import se.chalmers.tda367.std.utilities.Sprite;
 import se.chalmers.tda367.std.utilities.SpriteCreator;
@@ -11,11 +17,15 @@ import se.chalmers.tda367.std.utilities.SpriteCreator;
  */
 public class PlayerCharacter implements IPlayerCharacter {
 	private Position pos;
+	private int attackDmg, attackSpd, timeSinceLastAttack;
 	
 	private final static Sprite sprite = SpriteCreator.create("/images/gameplay/enemy.png");
 
 	public PlayerCharacter(Position pos) {
 		this.pos = pos;
+		attackDmg = 10;
+		timeSinceLastAttack = 0;
+		attackSpd = 150;
 	}
 	
 	@Override
@@ -31,5 +41,26 @@ public class PlayerCharacter implements IPlayerCharacter {
 	@Override
 	public Sprite getSprite() {
 		return sprite;
+	}
+	
+	@Override
+	public boolean isAttackReady(final int delta) {
+		timeSinceLastAttack += delta;
+		if(timeSinceLastAttack >= attackSpd){
+			timeSinceLastAttack = 0;
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public void shoot(List<IEnemy> enemies) {
+		if(!enemies.isEmpty()){
+			IEnemy enemy = enemies.get(0);
+			
+			enemy.decreaseHealth(attackDmg);
+		
+			EventBus.INSTANCE.post(new TowerShootingEvent(pos, enemy.getPosition()));
+		}
 	}
 }
