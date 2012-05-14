0b7c42c99a9b2e2f70a9c8a7926ff395d4b7c37c
Johan Gustafsson <johan.gusten@gmail.com>
2012-05-14 14:09:49 +0200

    Cleaned up MovementEnum even more

--- src/se/chalmers/tda367/std/core/MovementEnum.java
+++ src/se/chalmers/tda367/std/core/MovementEnum.java
@@ -4,42 +4,47 @@ import se.chalmers.tda367.std.utilities.Position;
 
 public enum MovementEnum {
 	MOVE_UP {
-	@Override
-	void moveCharacter(Position p, int delta) {
-		if(p.getY() > 1) {
-			p.decrementY();
-		};
-	} },
+		@Override
+		void moveCharacter(Position p, int delta) {
+			if(p.getY() > 1) {
+				p.decrementY();
+			};
+		} 
+	},
 	
 	MOVE_DOWN {
-	@Override
-	void moveCharacter(Position p, int delta) {
-		if(p.getY() < 639) {
-			p.incrementY();
-		};
-	} },
+		@Override
+		void moveCharacter(Position p, int delta) {
+			if(p.getY() < 639) {
+				p.incrementY();
+			};
+		} 
+	},
 	
 	MOVE_RIGHT {
-	@Override
-	void moveCharacter(Position p, int delta) {
-		if(p.getX() < 799) {
-			p.incrementX();
-		};
-	} },
+		@Override
+		void moveCharacter(Position p, int delta) {
+			if(p.getX() < 799) {
+				p.incrementX();
+			};
+		} 
+	},
 	
 	MOVE_LEFT {
-	@Override
-	void moveCharacter(Position p, int delta) {
-		if(p.getX() > 1) {
-			p.decrementX();
-		};
-	} },
+		@Override
+		void moveCharacter(Position p, int delta) {
+			if(p.getX() > 1) {
+				p.decrementX();
+			};
+		}
+	},
 	
-	NO_MOVEMENT { { }
-	@Override
-	void moveCharacter(Position p, int delta) {
-		
-	} };
+	NO_MOVEMENT {
+		@Override
+		void moveCharacter(Position p, int delta) {
+			
+		} 
+	};
 	
 	abstract void moveCharacter(Position p, int delta);
 }
