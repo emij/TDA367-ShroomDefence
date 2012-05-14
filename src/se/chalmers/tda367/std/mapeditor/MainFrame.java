b8e1c213dacd3cb3835f186e9d11d72ebab02f7a
Emil Edholm <emil@edholm.it>
2012-05-14 15:37:41 +0200

    Improved readabillity on MainFrame.java

--- src/se/chalmers/tda367/std/mapeditor/MainFrame.java
+++ src/se/chalmers/tda367/std/mapeditor/MainFrame.java
@@ -21,7 +21,6 @@ import java.awt.event.ActionEvent;
 import java.awt.CardLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.border.BevelBorder;
 
 /**
  * The main frame of the Map Editor.
@@ -60,11 +59,16 @@ public final class MainFrame extends JFrame {
 	
 	
 	public MainFrame(){
+		this.setTitle("STD Map Editor");
+	    this.setLocationRelativeTo(null); // Center screen
+	    this.setSize(1000,880);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		// Register this class to the eventbus.
 		EventBus.INSTANCE.register(this);
 		
 		initializeFrame();
+		initMenus();
 	}
 	
 	private void initializeFrame(){
@@ -79,64 +83,14 @@ public final class MainFrame extends JFrame {
 	    nothingLoadedPanel.add(lblNewLabel);
 	    getContentPane().add(splitPane, "name_30931601367131");
 	    
-	    JPanel leftPanel = new JPanel();
-	    splitPane.setLeftComponent(leftPanel);
-	    leftPanel.setLayout(new BorderLayout(0, 0));
-	    
-	    
-	    JPanel panel = new JPanel();
-	    leftPanel.add(panel, BorderLayout.WEST);
-	    GridBagLayout gbl_panel = new GridBagLayout();
-	    gbl_panel.rowHeights = new int[]{23, 0, 0, 0, 0, 0, 0, 0};
-	    gbl_panel.columnWeights = new double[]{1.0};
-	    gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-	    panel.setLayout(gbl_panel);
-	    
-	    
-	    JLabel lblChooseTile = new JLabel("Choose tile");
-	    GridBagConstraints gbc_lblChooseTile = new GridBagConstraints();
-	    gbc_lblChooseTile.insets = new Insets(0, 0, 5, 0);
-	    gbc_lblChooseTile.gridx = 0;
-	    gbc_lblChooseTile.gridy = 0;
-	    panel.add(lblChooseTile, gbc_lblChooseTile);
-	    lblChooseTile.setFont(new Font("Tahoma", Font.BOLD, 12));
-	    
-	    JComboBox<PlaceableTile> comboBox = new JComboBox<PlaceableTile>();
-	    comboBox.addActionListener(new ActionListener() {
-	    	public void actionPerformed(ActionEvent e) {
-	    		@SuppressWarnings("rawtypes")
-				JComboBox src = (JComboBox)e.getSource();
-	    		
-	    		// Update the selected tile.
-	    		selectedTile = (PlaceableTile)src.getSelectedItem();
-	    	}
-	    });
-	    comboBox.setModel(new DefaultComboBoxModel<PlaceableTile>(PlaceableTile.values()));
-	    GridBagConstraints gbc_comboBox = new GridBagConstraints();
-	    gbc_comboBox.insets = new Insets(0, 0, 5, 0);
-	    gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-	    gbc_comboBox.gridx = 0;
-	    gbc_comboBox.gridy = 1;
-	    panel.add(comboBox, gbc_comboBox);
-	    
-	    JLabel lblPreview = new JLabel("Preview");
-	    lblPreview.setFont(new Font("Tahoma", Font.BOLD, 12));
-	    GridBagConstraints gbc_lblPreview = new GridBagConstraints();
-	    gbc_lblPreview.insets = new Insets(0, 0, 5, 0);
-	    gbc_lblPreview.gridx = 0;
-	    gbc_lblPreview.gridy = 2;
-	    panel.add(lblPreview, gbc_lblPreview);
-	    
-	    JPanel previewPanel = new JPanel();
-	    previewPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-	    GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-	    gbc_panel_1.insets = new Insets(0, 0, 5, 0);
-	    gbc_panel_1.fill = GridBagConstraints.BOTH;
-	    gbc_panel_1.gridx = 0;
-	    gbc_panel_1.gridy = 3;
-	    panel.add(previewPanel, gbc_panel_1);
-	    
-	    MapJPanel mapPanel = new MapJPanel();
+	   
+	    initLeftSide();
+	    initRightSide();
+	}
+	
+	/** Init the right side that contains the actual map editor. */
+	private void initRightSide() {
+		MapJPanel mapPanel = new MapJPanel();
 	    mapPanel.addMouseListener(new MouseAdapter() {
 	    	@Override
 	    	public void mouseClicked(MouseEvent e) {
@@ -152,10 +106,51 @@ public final class MainFrame extends JFrame {
 	    });
 	    
 	    splitPane.setRightComponent(mapPanel);
-	    this.setTitle("STD Map Editor");
-	    this.setLocationRelativeTo(null); // Center screen
-	    this.setSize(1000,880);
+	}
+	
+	/** Init the split pane left side that contains options */
+	private void initLeftSide() {
+		JPanel leftPanel = new JPanel();
+	    splitPane.setLeftComponent(leftPanel);
+	    leftPanel.setLayout(new BorderLayout(0, 0));
 	    
+	    JPanel placementPanel = new JPanel();
+	    leftPanel.add(placementPanel, BorderLayout.WEST);
+	    GridBagLayout gbl_placementPanel = new GridBagLayout();
+	    gbl_placementPanel.rowHeights = new int[]{23, 0, 0, 0, 0, 0, 0, 0};
+	    gbl_placementPanel.columnWeights = new double[]{1.0};
+	    gbl_placementPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+	    placementPanel.setLayout(gbl_placementPanel);
+	    
+	    JLabel lblChooseTile = new JLabel("Choose tile");
+	    GridBagConstraints gbc_lblChooseTile = new GridBagConstraints();
+	    gbc_lblChooseTile.insets = new Insets(0, 0, 5, 0);
+	    gbc_lblChooseTile.gridx = 0;
+	    gbc_lblChooseTile.gridy = 0;
+	    placementPanel.add(lblChooseTile, gbc_lblChooseTile);
+	    lblChooseTile.setFont(new Font("Tahoma", Font.BOLD, 12));
+	    
+	    JComboBox<PlaceableTile> tileChooserComboBox = new JComboBox<PlaceableTile>();
+	    tileChooserComboBox.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    		@SuppressWarnings("rawtypes")
+				JComboBox src = (JComboBox)e.getSource();
+	    		
+	    		// Update the selected tile.
+	    		selectedTile = (PlaceableTile)src.getSelectedItem();
+	    	}
+	    });
+	    tileChooserComboBox.setModel(new DefaultComboBoxModel<PlaceableTile>(PlaceableTile.values()));
+	    GridBagConstraints gbc_comboBox = new GridBagConstraints();
+	    gbc_comboBox.insets = new Insets(0, 0, 5, 0);
+	    gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+	    gbc_comboBox.gridx = 0;
+	    gbc_comboBox.gridy = 1;
+	    placementPanel.add(tileChooserComboBox, gbc_comboBox);
+	}
+	
+	/** Initialize the menu system */
+	private void initMenus() {
 	    JMenuBar menuBar = new JMenuBar();
 	    setJMenuBar(menuBar);
 	    
