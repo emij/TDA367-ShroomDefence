7813021c90751a652c1d59025cfa3fbec07e1a09
Emil Edholm <emil@edholm.it>
2012-05-21 23:59:54 +0200

    Tweaked the balance a bit and added 2 new images

--- src/se/chalmers/tda367/std/core/exported/BossEnemy.java
+++ src/se/chalmers/tda367/std/core/exported/BossEnemy.java
@@ -15,15 +15,14 @@ import se.chalmers.tda367.std.utilities.Sprite;
  * @author Johan Gustafsson
  * @date   19 apr 2012
  */
-@Enemy(name = "El Jefe", description = "Represents a boss enemy.", enemyStrength = 10)
+@Enemy(name = "El Jefe", description = "Represents a boss enemy.", enemyStrength = 4)
 public class BossEnemy extends AbstractEnemy {
-	//TODO: change sprite for the boss enemy.
-	private final static Sprite sprite = SpriteCreator.create("/images/gameplay/enemy.png");
-	/**
-	 *  Creates a boss enemy.
-	 */
+	
+	private final static Sprite sprite = SpriteCreator.create("/images/gameplay/boss_enemy.png");
+	
+	/** Creates a boss enemy. */
 	public BossEnemy() {
-		super(400, 0.083F, 250, 10, sprite); //TODO: change sprite
+		super(400, 0.083F, 7, 250, sprite);
 	}
 	
 	/**
