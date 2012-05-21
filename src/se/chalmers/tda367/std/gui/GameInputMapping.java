9095396de2a7e48bd7653f794259d246ff30acf5
Johan Gustafsson <johan.gusten@gmail.com>
2012-05-21 15:02:02 +0200

    Forgot to add the file in the last commit.

---
+++ src/se/chalmers/tda367/std/gui/GameInputMapping.java
@@ -0,0 +1,19 @@
+package se.chalmers.tda367.std.gui;
+
+import de.lessvoid.nifty.input.NiftyInputEvent;
+import de.lessvoid.nifty.input.NiftyInputMapping;
+import de.lessvoid.nifty.input.keyboard.KeyboardInputEvent;
+
+/**
+ * Custom inputmapping class for overriding the default input mapping of 
+ * the space key. This solves nifty from activating elements when the player jumps.
+ * @author Johan Gustafsson
+ * @date   May 14, 2012
+ */
+public class GameInputMapping implements NiftyInputMapping {
+
+	@Override
+	public NiftyInputEvent convert(final KeyboardInputEvent inputEvent) {
+		return null;
+	}
+}
